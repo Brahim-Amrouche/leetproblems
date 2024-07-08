9e4927884ba0f9c7d1bd959462ff620dcbe0f69f
Brahim Amrouch <amroucheb4@gmail.com>
2024-07-08 17:30:44 +0100

    min_in_sorted_array

---
+++ src/bin/min_in_sorted_array.rs
@@ -0,0 +1,21 @@
+pub mod Soluion {
+
+    pub fn find_min(nums: &[i32]) -> i32 {
+        let mut left  = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let mid = (left + right) / 2;
+            if nums[mid] < nums[right] {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
+
+fn main() {
+
+}
